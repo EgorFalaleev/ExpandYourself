b12be00889c49e746a0d45da58e3389395a1973d
Egor <Egor1268@gmail.com>
2020-03-14 19:10:45 +0300

    Made the player increase on a value depending on the scale of the pickup

--- ExpandYourself/Assets/Scripts/Pickup.cs
+++ ExpandYourself/Assets/Scripts/Pickup.cs
@@ -39,6 +39,7 @@ public class Pickup : MonoBehaviour
         var scaleDifferenceY = transform.localScale.y / scalePerFrameDivideFactor;
 
         transform.localScale = new Vector2(transform.localScale.x - scaleDifferenceX, transform.localScale.y - scaleDifferenceY);
+        circleScale = transform.localScale;
 
         // destroy pickup if it becomes too small
         if (transform.localScale.x < scaleToDestroy || transform.localScale.y < scaleToDestroy)
