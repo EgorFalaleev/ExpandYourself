c07f094ff4c235acf04ca19b979588b948673f4b
Egor <Egor1268@gmail.com>
2020-03-18 09:55:24 +0300

    Implemented restart mechanism

--- ExpandYourself/Assets/Scripts/SceneLoader.cs
+++ ExpandYourself/Assets/Scripts/SceneLoader.cs
@@ -7,4 +7,9 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene("GameOver");
     }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
 }
