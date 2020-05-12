ab35d84724210f2c2a068241098f7586fab27dc8
Egor <xEgorFalaleev@yandex.ru>
2020-05-12 21:02:50 +0500

    Removed player destruction particle effect, tuned other particle effects

--- ExpandYourself/Assets/Scripts/NormalPickupParticlesHandler.cs
+++ ExpandYourself/Assets/Scripts/NormalPickupParticlesHandler.cs
@@ -7,11 +7,14 @@ public class NormalPickupParticlesHandler : MonoBehaviour
 
     void Start()
     {
-        // if multiplier is incremented increase number of particles generated
+        // if multiplier is incremented increase number of particles generated and their size
         if (needToIncrease)
         {
             ParticleSystem particles = GetComponent<ParticleSystem>();
             var particlesEmission = particles.emission;
+            var mainModule = particles.main;
+            mainModule.startSize = Random.Range(0.25f, 0.35f);
+            mainModule.startLifetime = Random.Range(0.5f, 1f);
             particlesEmission.SetBurst(0, new ParticleSystem.Burst(0, 100));
         }
     }
