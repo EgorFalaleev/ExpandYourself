c70d5c3aa0a5382dda82d4e2ac6013c14f226002
Egor <xEgorFalaleev@yandex.ru>
2020-05-22 22:07:21 +0500

    Added mechanism to show unlocked backgrounds and lock blocked backgrounds

--- ExpandYourself/Assets/Scripts/BackgroundsUnlocker.cs
+++ ExpandYourself/Assets/Scripts/BackgroundsUnlocker.cs
@@ -28,16 +28,64 @@ public class BackgroundsUnlocker : MonoBehaviour
 
         highScoreText.text = $"Your high score is {highScore}!";
 
+        // turn on unlocked starships and turn off blocked starships
+        ShowUnlockedBackgrounds(highScore);
+
         // find the current selected background
         chosenBackground = PlayerPrefs.GetInt("ChosenBackgroundNumber", 0);
-        currentBackground = GameObject.Find(GetStarshipName((Backgrounds)chosenBackground));
+        currentBackground = GameObject.Find(GetBackgroundName((Backgrounds)chosenBackground));
 
         // color selected background to orange and prevent it to being clicked
         currentBackground.GetComponent<Image>().color = new Color(0.91f, 0.7f, 0.435f, 0.537f);
         currentBackground.GetComponent<Button>().interactable = false;
     }
 
-    private string GetStarshipName(Backgrounds background)
+    private void ShowUnlockedBackgrounds(int highScore)
+    {
+        GameObject backgroundToCheck;
+        GameObject backgroundImage;
+
+        // for each starship, check if it is unlocked and color it and turn on/off
+        for (int i = 0; i < 4; i++)
+        {
+            backgroundToCheck = GameObject.Find(GetBackgroundName((Backgrounds)i));
+            backgroundImage = GetBackgroundImage(i);
+
+            if (highScore >= GetBackgroundCost(i))
+            {
+                backgroundImage.GetComponent<Image>().color = Color.white;
+            }
+            else
+            {
+                backgroundImage.GetComponent<Image>().color = Color.black;
+                backgroundToCheck.GetComponent<Button>().interactable = false;
+            }
+        }
+    }
+
+    private int GetBackgroundCost(int starshipNumber)
+    {
+        switch (starshipNumber)
+        {
+            case 1: return 200;
+            case 2: return 350;
+            case 3: return 500;
+            default: return 0;
+        }
+    }
+
+    private GameObject GetBackgroundImage(int starshipNumber)
+    {
+        switch (starshipNumber)
+        {
+            case 1: return GameObject.Find("200 background Image");
+            case 2: return GameObject.Find("300 background Image");
+            case 3: return GameObject.Find("500 background Image");
+            default: return GameObject.Find("Starting Background Image");
+        }
+    }
+
+    private string GetBackgroundName(Backgrounds background)
     {
         switch (background)
         {
@@ -54,7 +102,7 @@ public class BackgroundsUnlocker : MonoBehaviour
         currentBackground.GetComponent<Button>().interactable = true;
         currentBackground.GetComponent<Image>().color = new Color(1, 1, 1, 0.537f);
 
-        currentBackground = GameObject.Find(GetStarshipName((Backgrounds)backgroundNumber));
+        currentBackground = GameObject.Find(GetBackgroundName((Backgrounds)backgroundNumber));
 
         // color selected background to orange and prevent it to being clicked
         currentBackground.GetComponent<Image>().color = new Color(0.91f, 0.7f, 0.435f, 0.537f);
