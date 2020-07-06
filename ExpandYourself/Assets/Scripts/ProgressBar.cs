cf6851e7a70392c2026ac8d2de1638f00c765981
Egor <xEgorFalaleev@yandex.ru>
2020-07-06 19:26:44 +0500

    Added progress bars to unlocks scene

---
+++ ExpandYourself/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    // configuration parameters
+    [SerializeField] Slider slider;
+    [SerializeField] bool isTotalPickupsChecked = true;
+
+    private void Start()
+    {
+        // set the value of progress bar depending on the property it requires
+        if (isTotalPickupsChecked) SetScore(PlayerPrefs.GetInt("TotalPickups", 0));
+        else SetScore(PlayerPrefs.GetInt("HighScore", 0));
+    }
+
+    public void SetScore(int score)
+    {
+        if (score > slider.maxValue) Destroy(gameObject);
+
+        slider.value = score;        
+    }
+}
