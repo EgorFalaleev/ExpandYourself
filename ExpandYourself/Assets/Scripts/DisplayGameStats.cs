7822de439fffa35fd3a999a178d7a2fa24cf6b72
Egor <xEgorFalaleev@yandex.ru>
2020-07-19 18:05:00 +0500

    Changed game over screen (game over text and score text)

--- ExpandYourself/Assets/Scripts/DisplayGameStats.cs
+++ ExpandYourself/Assets/Scripts/DisplayGameStats.cs
@@ -5,14 +5,18 @@ public class DisplayGameStats : MonoBehaviour
 {
     // configuration parameters
     [SerializeField] Text scoreText;
-    [SerializeField] Text highScoreText;
     [SerializeField] Text totalPickupsText;
+    [SerializeField] Text loseText;
 
     void Start()
     {
         // display player stats
-        scoreText.text = "Your score: " + GameStatsHolder.Instance.GetScore().ToString();
-        highScoreText.text = "High score: " + PlayerPrefs.GetInt("HighScore");
-        totalPickupsText.text = "Total pickups collected: " + PlayerPrefs.GetInt("TotalPickups");
+        scoreText.text = "Your score: " + GameStatsHolder.Instance.GetScore().ToString() + " (Best: " + PlayerPrefs.GetInt("HighScore") + ")";
+        totalPickupsText.text = "Total gears collected: " + PlayerPrefs.GetInt("TotalPickups");
+
+        // display lose text depending on player score
+        if (GameStatsHolder.Instance.GetScore() <= 100) loseText.text = "Better luck next time!";
+        else if (GameStatsHolder.Instance.GetScore() <= 350) loseText.text = "Well done!";
+        else loseText.text = "You are awesome!";
     }
 }
